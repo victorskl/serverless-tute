8d9be645fd9167430b9d90fcaa7349da2f726f66
Victor San Kho Lin <victor@sankholin.com>
2019-11-13 13:06:53 +1100

    Added Login page and process login with AWS Cognito using Amplify

---
+++ notes-app-client/src/containers/Login.js
@@ -0,0 +1,53 @@
+// https://serverless-stack.com/chapters/create-a-login-page.html
+import React, { useState } from "react";
+import { Button, FormGroup, FormControl, ControlLabel } from "react-bootstrap";
+import "./Login.css";
+import { Auth } from "aws-amplify";
+
+export default function Login(props) {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    function validateForm() {
+        return email.length > 0 && password.length > 0;
+    }
+
+    async function handleSubmit(event) {
+        event.preventDefault();
+
+        // https://serverless-stack.com/chapters/login-with-aws-cognito.html
+        try {
+            await Auth.signIn(email, password);
+            alert("Logged in");
+        } catch (e) {
+            alert(e.message);
+        }
+    }
+
+    return (
+        <div className="Login">
+            <form onSubmit={handleSubmit}>
+                <FormGroup controlId="email" bsSize="large">
+                    <ControlLabel>Email</ControlLabel>
+                    <FormControl
+                        autoFocus
+                        type="email"
+                        value={email}
+                        onChange={e => setEmail(e.target.value)}
+                    />
+                </FormGroup>
+                <FormGroup controlId="password" bsSize="large">
+                    <ControlLabel>Password</ControlLabel>
+                    <FormControl
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        type="password"
+                    />
+                </FormGroup>
+                <Button block bsSize="large" disabled={!validateForm()} type="submit">
+                    Login
+                </Button>
+            </form>
+        </div>
+    );
+}
