a1bcd68064685e11dd64fbd4e85b73efbd7bcfee
Victor San Kho Lin <victor@sankholin.com>
2019-11-21 15:40:05 +1100

    Signup with AWS Cognito

--- notes-app-client/src/containers/Signup.js
+++ notes-app-client/src/containers/Signup.js
@@ -8,6 +8,7 @@ import {
 import LoaderButton from "../components/LoaderButton";
 import { useFormFields } from "../libs/hooksLib";
 import "./Signup.css";
+import { Auth } from "aws-amplify";
 
 /**
  * Create the Signup Form
@@ -39,20 +40,68 @@ export default function Signup(props) {
         return fields.confirmationCode.length > 0;
     }
 
+    /**
+     * Signup with AWS Cognito
+     * https://serverless-stack.com/chapters/signup-with-aws-cognito.html
+     *
+     * The flow here is pretty simple:
+     * 1. In handleSubmit we make a call to signup a user. This creates a new user object.
+     * 2. Save that user object to the state using setNewUser.
+     * 3. In handleConfirmationSubmit use the confirmation code to confirm the user.
+     * 4. With the user now confirmed, Cognito now knows that we have a new user that can login to our app.
+     * 5. Use the email and password to authenticate exactly the same way we did in the login page.
+     * 6. Update the App’s state using the userHasAuthenticated function.
+     * 7. Finally, redirect to the homepage.
+     *
+     * @param event
+     * @returns {Promise<void>}
+     */
     async function handleSubmit(event) {
         event.preventDefault();
 
         setIsLoading(true);
 
-        setNewUser("test");
-
-        setIsLoading(false);
+        try {
+            const newUser = await Auth.signUp({
+                username: fields.email,
+                password: fields.password
+            });
+            setIsLoading(false);
+            setNewUser(newUser);
+        } catch (e) {
+            alert(e.message);
+            setIsLoading(false);
+        }
     }
 
+    /**
+     * A quick note on the signup flow here. If the user refreshes their page at the confirm step, they won't be able
+     * to get back and confirm that account. It forces them to create a new account instead. We are keeping things
+     * intentionally simple but here are a couple of hints on how to fix it.
+     * 1. Check for the UsernameExistsException in the handleSubmit function’s catch block.
+     * 2. Use the Auth.resendSignUp() method to resend the code if the user has not been previously confirmed.
+     *      Here is a link to the Amplify API docs. https://aws-amplify.github.io/amplify-js/api/classes/authclass.html#resendsignup
+     * 3. Confirm the code just as we did before.
+     *
+     * @param event
+     * @returns {Promise<void>}
+     */
     async function handleConfirmationSubmit(event) {
         event.preventDefault();
 
         setIsLoading(true);
+
+        // Signup with AWS Cognito
+        try {
+            await Auth.confirmSignUp(fields.email, fields.confirmationCode);
+            await Auth.signIn(fields.email, fields.password);
+
+            props.userHasAuthenticated(true);
+            props.history.push("/");
+        } catch (e) {
+            alert(e.message);
+            setIsLoading(false);
+        }
     }
 
     function renderConfirmationForm() {
