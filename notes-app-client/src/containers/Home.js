5226060bfa356a45cc26931fe300bcc92c9003dc
Victor San Kho Lin <victor@sankholin.com>
2019-11-21 16:24:30 +1100

    List All the Notes and Call the List API

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -1,3 +1,4 @@
+/*
 import React from "react";
 import "./Home.css";
 
@@ -8,6 +9,97 @@ export default function Home() {
                 <h1>Scratch</h1>
                 <p>A simple note taking app</p>
             </div>
+        </div>
+    );
+}
+*/
+import React, { useState, useEffect } from "react";
+import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
+import "./Home.css";
+import { API } from "aws-amplify";
+
+/**
+ * List All the Notes
+ * https://serverless-stack.com/chapters/list-all-the-notes.html
+ *
+ * Call the List API
+ * https://serverless-stack.com/chapters/call-the-list-api.html
+ *
+ * @param props
+ * @returns {*}
+ * @constructor
+ */
+export default function Home(props) {
+    const [notes, setNotes] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        async function onLoad() {
+            if (!props.isAuthenticated) {
+                return;
+            }
+
+            try {
+                const notes = await loadNotes();
+                setNotes(notes);
+            } catch (e) {
+                alert(e);
+            }
+
+            setIsLoading(false);
+        }
+
+        onLoad();
+    }, [props.isAuthenticated]);
+
+    function loadNotes() {
+        return API.get("notes", "/notes");
+    }
+
+    function renderNotesList(notes) {
+        return [{}].concat(notes).map((note, i) =>
+            i !== 0 ? (
+                <LinkContainer key={note.noteId} to={`/notes/${note.noteId}`}>
+                    <ListGroupItem header={note.content.trim().split("\n")[0]}>
+                        {"Created: " + new Date(note.createdAt).toLocaleString()}
+                    </ListGroupItem>
+                </LinkContainer>
+            ) : (
+                <LinkContainer key="new" to="/notes/new">
+                    <ListGroupItem>
+                        <h4>
+                            <b>{"\uFF0B"}</b> Create a new note
+                        </h4>
+                    </ListGroupItem>
+                </LinkContainer>
+            )
+        );
+    }
+
+    function renderLander() {
+        return (
+            <div className="lander">
+                <h1>Scratch</h1>
+                <p>A simple note taking app</p>
+            </div>
+        );
+    }
+
+    function renderNotes() {
+        return (
+            <div className="notes">
+                <PageHeader>Your Notes</PageHeader>
+                <ListGroup>
+                    {!isLoading && renderNotesList(notes)}
+                </ListGroup>
+            </div>
+        );
+    }
+
+    return (
+        <div className="Home">
+            {props.isAuthenticated ? renderNotes() : renderLander()}
         </div>
     );
 }
