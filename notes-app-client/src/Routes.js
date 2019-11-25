9e45a25fac42d2f904f5dab2528a0324cf0fff2c
Victor San Kho Lin <victor@sankholin.com>
2019-11-25 15:35:54 +1100

    Use the Redirect Routes

--- notes-app-client/src/Routes.js
+++ notes-app-client/src/Routes.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import AppliedRoute from "./components/AppliedRoute";
+import AuthenticatedRoute from "./components/AuthenticatedRoute";
+import UnauthenticatedRoute from "./components/UnauthenticatedRoute";
 import Home from "./containers/Home";
 import NotFound from "./containers/NotFound";
 import Login from "./containers/Login";
@@ -12,10 +14,10 @@ export default function Routes({ appProps }) {
     return (
         <Switch>
             <AppliedRoute path="/" exact component={Home} appProps={appProps} />
-            <AppliedRoute path="/login" exact component={Login} appProps={appProps} />
-            <AppliedRoute path="/signup" exact component={Signup} appProps={appProps} />
-            <AppliedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
-            <AppliedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
+            <UnauthenticatedRoute path="/login" exact component={Login} appProps={appProps} />
+            <UnauthenticatedRoute path="/signup" exact component={Signup} appProps={appProps} />
+            <AuthenticatedRoute path="/notes/new" exact component={NewNote} appProps={appProps} />
+            <AuthenticatedRoute path="/notes/:id" exact component={Notes} appProps={appProps} />
             { /* Finally, catch all unmatched routes */ }
             <Route component={NotFound} />
         </Switch>
