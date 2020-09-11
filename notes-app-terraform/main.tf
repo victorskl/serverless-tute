2bdbf8fc9af4e06afca37e1c40cc8521b7d1d4a6
Victor San Kho Lin <victor@sankholin.com>
2020-09-11 16:52:33 +1000

    Updated to terraform 0.12

--- notes-app-terraform/main.tf
+++ notes-app-terraform/main.tf
@@ -1,10 +1,14 @@
 terraform {
-  required_version = "~> 0.11.6"
+  required_version = ">= 0.12"
+}
+
+provider "template" {
+  version = "~> 2.1.2"
 }
 
 provider "aws" {
   version = "~> 2.6"
-  region = "${var.region}"
+  region = var.region
 }
 
 data "aws_caller_identity" "current" {}
@@ -48,14 +52,14 @@ resource "aws_cognito_user_pool" "pool" {
 
 resource "aws_cognito_user_pool_client" "client" {
   name = "notesapp-client"
-  user_pool_id = "${aws_cognito_user_pool.pool.id}"
+  user_pool_id = aws_cognito_user_pool.pool.id
   generate_secret = false
   explicit_auth_flows = ["ADMIN_NO_SRP_AUTH"]
 }
 
 resource "aws_cognito_user_pool_domain" "domain" {
   domain = "notesapp-dev"
-  user_pool_id = "${aws_cognito_user_pool.pool.id}"
+  user_pool_id = aws_cognito_user_pool.pool.id
 }
 
 // https://serverless-stack.com/chapters/create-a-cognito-identity-pool.html
@@ -64,43 +68,43 @@ resource "aws_cognito_identity_pool" "this" {
   allow_unauthenticated_identities = false
 
   cognito_identity_providers {
-    client_id = "${aws_cognito_user_pool_client.client.id}"
-    provider_name = "${aws_cognito_user_pool.pool.endpoint}"
+    client_id = aws_cognito_user_pool_client.client.id
+    provider_name = aws_cognito_user_pool.pool.endpoint
     server_side_token_check = false
   }
 }
 
 # Assume role policy for authenticated identities
 data "template_file" "iam_role_authenticated_assume_role_policy" {
-  template = "${file("policies/iam_role_authenticated_assume_role_policy.json")}"
+  template = file("policies/iam_role_authenticated_assume_role_policy.json")
 
-  vars {
-    identity_pool_id = "${aws_cognito_identity_pool.this.id}"
+  vars = {
+    identity_pool_id = aws_cognito_identity_pool.this.id
   }
 }
 
 resource "aws_iam_role" "notesapp_auth_role" {
   name = "notesapp-auth-role"
-  assume_role_policy = "${data.template_file.iam_role_authenticated_assume_role_policy.rendered}"
+  assume_role_policy = data.template_file.iam_role_authenticated_assume_role_policy.rendered
 }
 
 # Resource permission policy for authenticated identities
 data "template_file" "iam_role_authenticated_policy" {
   # FIXME: should have a explicit reference to our api
-  template = "${file("policies/iam_role_authenticated_policy.json")}"
+  template = file("policies/iam_role_authenticated_policy.json")
 }
 
 resource "aws_iam_role_policy" "role_policy_authenticated" {
   name = "notesapp-authenticated-policy"
-  role = "${aws_iam_role.notesapp_auth_role.id}"
-  policy = "${data.template_file.iam_role_authenticated_policy.rendered}"
+  role = aws_iam_role.notesapp_auth_role.id
+  policy = data.template_file.iam_role_authenticated_policy.rendered
 }
 
 # Attach the IAM role to the identity pool
 resource "aws_cognito_identity_pool_roles_attachment" "identity_pool_role_attach" {
-  identity_pool_id = "${aws_cognito_identity_pool.this.id}"
+  identity_pool_id = aws_cognito_identity_pool.this.id
 
   roles = {
-    "authenticated" = "${aws_iam_role.notesapp_auth_role.arn}"
+    "authenticated" = aws_iam_role.notesapp_auth_role.arn
   }
 }
