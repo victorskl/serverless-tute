7cafc710497b6abbe27fbaaefddec4abe6825224
Victor San Kho Lin <victor@sankholin.com>
2024-05-19 23:19:00 +1000

    Removed Serverless CRA based notes-app-client

--- sls/notes-app-client/src/components/AppliedRoute.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Route } from "react-router-dom";
-
-// create a new component that’ll extend the standard Route component and apply the appProps.
-export default function AppliedRoute({ component: C, appProps, ...rest }) {
-    return (
-        <Route {...rest} render={props => <C {...props} {...appProps} />} />
-    );
-}
